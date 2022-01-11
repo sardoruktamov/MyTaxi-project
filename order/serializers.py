13f799e98b48e9a7ccf699cc1e469966b7ee17c0
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-01-11 17:58:43 +0500

    OrderListSerializer, GetCustomerSerializer create

---
+++ order/serializers.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework.fields import ReadOnlyField
+from rest_framework.serializers import ModelSerializer
+from .models import Order
+from client.models import Client
+
+class GetCustomerSerializer(ModelSerializer):
+    class Meta:
+        model = Client
+        fields = "__all__"
+
+class OrderListSerializer(ModelSerializer):
+    client = GetCustomerSerializer(read_only=True, many=False)
+    class Meta:
+        model = Order
+        fields = ['client', 'created_at', 'update_at']
+        ReadOnlyField = ['created_at', 'update_at']
