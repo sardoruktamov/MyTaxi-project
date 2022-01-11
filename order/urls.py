b5795b196a63bd3bce1984cd1d21849dd834ae9b
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-01-11 22:56:17 +0500

    FilterOrderAPIView created

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateOrderAPIView, AcceptOrderAPIView, OrderListAPIView, OrderStatusUpdateAPIView
+from .views import CreateOrderAPIView, AcceptOrderAPIView, OrderListAPIView, OrderStatusUpdateAPIView, FilterOrderAPIView
 
 urlpatterns = [
     path('create/', CreateOrderAPIView.as_view()),
     path('accept/', AcceptOrderAPIView.as_view()),
     path('list/', OrderListAPIView.as_view()),
     path('<int:pk>/', OrderStatusUpdateAPIView.as_view()),
+    path('filter/<int:pk>/', FilterOrderAPIView.as_view()),
 ]
--- order/views.py
+++ order/views.py
@@ -2,11 +2,14 @@ from django.db.models.query import QuerySet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import serializers, status
+from rest_framework import generics
 from .models import Order, AcceptOrder, OrderStatus
 from client.models import Client
 from driver.models import Driver
-from .serializers import OrderListSerializer, OrderUpdateSerializer
+from .serializers import OrderListSerializer, OrderUpdateSerializer, AcceptOrderCreateSerializer
 from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
+from django.shortcuts import get_object_or_404
+import datetime
 
 #client tomonidan driver zakas qilish
 class CreateOrderAPIView(APIView):
@@ -18,8 +21,11 @@ class CreateOrderAPIView(APIView):
         return Response("Order is created!", status=status.HTTP_201_CREATED)
 
 #zakas holatining nazorat qismi  statuses: created, cancelled, accepted and finished
-class AcceptOrderAPIView(APIView):
+class AcceptOrderAPIView(generics.CreateAPIView):
+    queryset = AcceptOrder.objects.all()
+    serializer_class = AcceptOrderCreateSerializer
     def post(self, request):
+
         data = request.data
         driver = data['driver']
         client = data['client']
@@ -29,12 +35,32 @@ class AcceptOrderAPIView(APIView):
         get_client = Client.objects.get(id=client)
         get_status = OrderStatus.objects.get(id=status)
 
-        print(get_driver)
-        print(get_client)
-        print(get_status)
         accepted_order = AcceptOrder.objects.create(driver=get_driver, client=get_client, status=get_status)
+        serializer = AcceptOrderCreateSerializer(accepted_order, read_only=True)
         
-        return Response("AcceptOrder is accepted")
+        return Response({"status":"create accept"})
+
+class FilterOrderAPIView(generics.ListAPIView):
+    queryset = AcceptOrder.objects.all()
+    serializer_class = OrderListSerializer
+
+    def filter_queryset(self, queryset):
+        client = get_object_or_404(Client,id=self.kwargs['pk'])
+        if 'from' in self.request.GET and 'to' in self.request.GET:
+            from1 = self.request.GET['from']
+            to1 = self.request.GET['to']
+
+            from2 = (str(from1).split('/'))  #['5', '01', '2022']
+            to2 = (str(to1).split('/'))
+
+            from_date = datetime.datetime(int(from2[2]),int(from2[1]),int(from2[0]))  #2022-01-05 00:00:00
+            to_date = datetime.datetime(int(to2[2]),int(to2[1]),int(to2[0]))
+            queryset = AcceptOrder.objects.filter(client=client,update_at__range=(from_date, to_date))
+            return super().filter_queryset(queryset)
+        else:
+            queryset = Order.objects.filter(client=client)
+            return super().filter_queryset(queryset)
+
 
 class OrderListAPIView(ListAPIView):
     queryset = Order.objects.all()
@@ -42,4 +68,4 @@ class OrderListAPIView(ListAPIView):
 
 class OrderStatusUpdateAPIView(RetrieveUpdateAPIView):
     queryset = AcceptOrder.objects.all()
-    serializer_class = OrderListSerializer
+    serializer_class = OrderUpdateSerializer
