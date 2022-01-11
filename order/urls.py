aff247c5d2ae5ac835e742b4af357af7c99affbe
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-01-11 17:58:50 +0500

    OrderListAPIView

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateOrderAPIView, AcceptOrderAPIView
+from .views import CreateOrderAPIView, AcceptOrderAPIView, OrderListAPIView
 
 urlpatterns = [
     path('create/', CreateOrderAPIView.as_view()),
     path('accept/', AcceptOrderAPIView.as_view()),
+    path('list/', OrderListAPIView.as_view()),
 ]
