64fc78950888a374ff62442d001ff67f7cbf1c1f
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-01-14 17:21:36 +0500

    client get, post, put create

--- client/views.py
+++ client/views.py
@@ -1,14 +1,18 @@
+import imp
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Client
+from .serializers import ClientSerializer
 
-class CreateClientAPIView(APIView):
-    def post(self, request):
-        data = request.data
-        first_name = data['first_name']
-        last_name = data['last_name']
-        phone_number = data['phone_number']
-        client = Client.objects.create(first_name=first_name, last_name=last_name, phone_number=phone_number)
+#GET, POST
+class ClientListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
 
-        return Response("Client is created!", status=status.HTTP_201_CREATED)
+
+#GET_ID, PUT
+class RetrieveClientAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
