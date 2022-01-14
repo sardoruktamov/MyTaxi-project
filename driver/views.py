4c08e4249c0703611453fd25168d676f1fae0f19
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-01-14 17:21:54 +0500

    driver get, post, put create

--- driver/views.py
+++ driver/views.py
@@ -1,16 +1,18 @@
 from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Driver
+from .serializers import DriverSerializer
 
-class CreateDriverAPIView(APIView):
-    def post(self, request):
-        data = request.data
-        first_name = data['first_name']
-        last_name = data['last_name']
-        car_number = data['car_number']
-        phone_number = data['phone_number']
-        driver = Driver.objects.create(first_name=first_name, last_name=last_name,
-                                       car_number=car_number, phone_number=phone_number)
+#GET, POST
+class DrivetListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
 
-        return Response("Driver is created!", status=status.HTTP_201_CREATED)
+
+#GET_ID, PUT
+class RetrieveDriverAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+
